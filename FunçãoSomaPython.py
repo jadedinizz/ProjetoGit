998ef28ccb2537e839e77e5b679c054e86eb13b8
Jade <jadedinizpro@gmail.com>
2025-02-18 23:04:29 -0300

    função soma no pyhton

---
+++ FunçãoSomaPython.py
@@ -0,0 +1,16 @@
+import os 
+os.system('cls')
+
+def soma (a, b):
+  return a + b
+
+x = int(input("Digite um número: "))
+y = int(input("Digite outro número: "))
+
+
+soma_dos_numeros = soma(x, y)
+print("A soma dos números é: " , soma_dos_numeros)
+
+
+
+
